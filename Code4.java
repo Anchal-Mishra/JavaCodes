671b1f1951b00c05acdc730be884f9cd0ac024d1
Anchal-Mishra <166801709+Anchal-Mishra@users.noreply.github.com>
2024-04-12 16:32:17 +0530

    Add files via upload
    Sum of each digit of a 4 digit number

---
+++ Code4.java
@@ -0,0 +1,16 @@
+public class Code4 {
+    public static void main(String args[]){
+        int num=8965, rev, a,b,c,d;
+        a=num%10;
+        num=num/10;
+        b=num%10;
+        num=num/10;
+        c=num%10;
+        num=num/10;
+        d=num%10;
+        num=num/10;
+        rev=a*1000+b*100+c*10+d*1;
+        System.out.println("each digit :"+a+" "+b+" "+c+"  "+d);
+        System.out.println("Reverse Order : "+rev);
+    }
+}
